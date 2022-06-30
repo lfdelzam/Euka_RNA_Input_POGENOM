6a8d503aaded1a3c552876e3187ddcfceaef91d7
lfdelzam <48052238+lfdelzam@users.noreply.github.com>
2022-07-01 00:32:48 +0200

    parallelization

--- src/create_prefilt_Reads_subdir.sh
+++ src/create_prefilt_Reads_subdir.sh
@@ -7,36 +7,49 @@ err_report() {
 
 trap 'err_report $LINENO' ERR
 
+start=`date +%s.%N`
 #---arguments---
-
 wd=$(pwd)
 fract=$1
-mags_ext=$2
-reads_ext=$3
-dataset=$4
-Dts=$5
+reads_ext=$2
+dataset=$3
+Dts=$4
 
 #--- Main ----
 
 mkdir -p $dataset/Reads/fraction_$fract
 
+subsample_reads() {
+  r=$1
+  wd=$2
+  dataset=$3
+  fract=$4
+
+  read_file=$(basename $r)
+   if ! test -s $wd/$dataset/Reads/fraction_$fract/$read_file  #If file doesn't exit or if it exist but it is empty
+      then
+           all_lines_in=$(gzip -cd $r | wc -l)
+           all_number_reads=$( echo $all_lines_in/4 | bc )
+           subsample=$( echo $all_number_reads*$fract | bc )
+           seqtk sample -s100 $r $subsample > $wd/$dataset/Reads/fraction_$fract/temponame_$read_file
+           lines_in=$(cat $wd/$dataset/Reads/fraction_$fract/temponame_$read_file | wc -l)
+           number_reads=$( echo $lines_in/4 | bc )
+           gzip -c $wd/$dataset/Reads/fraction_$fract/temponame_$read_file > $wd/$dataset/Reads/fraction_$fract/$read_file
+           rm $wd/$dataset/Reads/fraction_$fract/temponame_$read_file
+           echo "      Subset $read_file created - Number of reads in subset: $number_reads"
+   else
+        echo "      Subset $read_file already created"
+   fi
+
+}
+
 Rds=($(ls $wd/RAW_DATA/Reads/$Dts/*$reads_ext))
 
 for r in "${Rds[@]}"
    do
-      read_file=$(basename $r)
-       if ! test -s $wd/$dataset/Reads/fraction_$fract/$read_file  #If file doesn't exit or if it exist but it is empty
-          then
-               all_lines_in=$(gzip -cd $r | wc -l)
-               all_number_reads=$( echo $all_lines_in/4 | bc )
-               subsample=$( echo $all_number_reads*$fract | bc )
-               seqtk sample -s100 $r $subsample > $wd/$dataset/Reads/fraction_$fract/temponame
-               lines_in=$(cat $wd/$dataset/Reads/fraction_$fract/temponame | wc -l)
-  	           number_reads=$( echo $lines_in/4 | bc )
-               gzip -c $wd/$dataset/Reads/fraction_$fract/temponame > $wd/$dataset/Reads/fraction_$fract/$read_file
-               rm $wd/$dataset/Reads/fraction_$fract/temponame
-               echo "      Subset $read_file created - Number of reads in subset: $number_reads"
-       else
-            echo "      Subset $read_file already created"
-       fi
+     subsample_reads "$r" "$wd" "$dataset" "$fract" &
    done
+wait
+end=`date +%s.%N`
+runtime=$( echo "$end - $start" | bc -l )
+echo "INFO: Subsampling reads done in $runtime (s)"
